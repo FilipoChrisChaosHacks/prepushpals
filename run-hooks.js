e43f11d2c41ce361df217afa2aa3293a1eb52298
filipomarcellino <filipo.marcellino@aquanow.io>
2024-03-02 13:42:08 -0800

    require fs

--- run-hooks.js
+++ run-hooks.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 const path = require('path');
-
+const fs = require('fs');
 function getGitRoot() {
     return execSync('git rev-parse --show-toplevel').toString().trim();
 }
